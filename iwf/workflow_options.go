5e5873d15184612cf9f1488c3becc26eeccf44e0
Quanzheng Long <qlong@indeed.com>
2023-01-20 13:25:57 -0800

    add comment

--- iwf/workflow_options.go
+++ iwf/workflow_options.go
@@ -9,5 +9,6 @@ type WorkflowOptions struct {
 	StartStateOptions     *iwfidl.WorkflowStateOptions
 	// InitialSearchAttributes set the initial search attributes to start a workflow
 	// key is search attribute key, value much match with PersistenceSchema of the workflow definition
+	// For iwfidl.DATETIME , the value can be either time.Time or a string value in format of DateTimeFormat
 	InitialSearchAttributes map[string]interface{}
 }
