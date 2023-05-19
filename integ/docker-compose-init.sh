70079844394d187e8ed8e116ad0fc6cdcc5a2591
Quanzheng Long <qlong@indeed.com>
2023-05-19 16:10:02 -0700

    Fix CI search attribute registration (#61)

--- integ/docker-compose-init.sh
+++ integ/docker-compose-init.sh
@@ -3,6 +3,7 @@
 # use new version of tctl so that we can skip the prompt
 tctl config set version next
 
+
 checkExists () {
   # see https://github.com/temporalio/temporal/issues/4160
   if tctl search-attribute list | grep -q "$1"; then
@@ -13,8 +14,7 @@ fi
 }
 
 echo "now trying to register iWF system search attributes..."
-
-for run in {1..60}; do
+for run in {1..120}; do
   sleep 1
   tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
   sleep 0.1
@@ -22,10 +22,23 @@ for run in {1..60}; do
   sleep 0.1
   tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
   sleep 0.1
-  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" ] ; then
-      echo "All search attributes are registered"
-      break
-    fi
+  tctl search-attribute  create -name CustomKeywordField -type Keyword -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomIntField -type Int -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomBoolField -type Bool -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomDoubleField -type Double -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomDatetimeField -type Datetime -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomStringField -type text -y
+
+  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" && checkExists "CustomKeywordField" && checkExists "CustomIntField" && checkExists "CustomBoolField" && checkExists "CustomDoubleField" && checkExists "CustomDatetimeField" && checkExists "CustomStringField" ] ; then
+    echo "All search attributes are registered"
+    break
+  fi
+
 done
 
 tctl namespace register default
